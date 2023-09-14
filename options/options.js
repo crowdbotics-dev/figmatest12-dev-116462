0a007cc9b4a0c1205d71751cd2ba3df55dc1f2f3
Crowdbotics <engineering@crowdbotics.com>
2023-09-14 13:55:34 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "figmatest12-dev-116462",
+  url: "https://figmatest12-dev-116462.botics.co",
+  api: "https://figmatest12-dev-116462.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
